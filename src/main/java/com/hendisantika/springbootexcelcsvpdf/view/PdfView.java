2784004abc07da068dcf1884294919a8f64fd012
Hendi Santika <hendisantika@gmail.com>
2018-02-20 06:28:35 +0700

    #9 - Add PDF View

---
+++ src/main/java/com/hendisantika/springbootexcelcsvpdf/view/PdfView.java
@@ -0,0 +1,91 @@
+package com.hendisantika.springbootexcelcsvpdf.view;
+
+import com.hendisantika.springbootexcelcsvpdf.model.User;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-excel-csv-pdf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/02/18
+ * Time: 06.21
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class PdfView extends AbstractPdfView {
+    @Override
+    protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // change the file name
+        response.setHeader("Content-Disposition", "attachment; filename=\"my-pdf-file.pdf\"");
+
+        List<User> users = (List<User>) model.get("users");
+        document.add(new Paragraph("Generated Users " + LocalDate.now()));
+
+        PdfPTable table = new PdfPTable(users.stream().findAny().get().getColumnCount());
+        table.setWidthPercentage(100.0f);
+        table.setSpacingBefore(10);
+
+        // define font for table header row
+        Font font = FontFactory.getFont(FontFactory.TIMES);
+        font.setColor(BaseColor.WHITE);
+
+        // define table header cell
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(BaseColor.DARK_GRAY);
+        cell.setPadding(5);
+
+        // write table header
+        cell.setPhrase(new Phrase("First Name", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Last Name", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Age", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Job Title", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Company", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Address", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("City", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Country", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Phone Number", font));
+        table.addCell(cell);
+
+        for (User user : users) {
+            table.addCell(user.getFirstName());
+            table.addCell(user.getLastName());
+            table.addCell(user.getAge().toString());
+            table.addCell(user.getJobTitle());
+            table.addCell(user.getCompany());
+            table.addCell(user.getAddress());
+            table.addCell(user.getCity());
+            table.addCell(user.getCountry());
+            table.addCell(user.getPhoneNumber());
+
+        }
+
+        document.add(table);
+    }
+}
