774f7c075be934bc7090e9d8e26d25d33239d922
Hendi Santika <hendisantika@gmail.com>
2018-02-20 06:32:14 +0700

    #14 - Add User Controller

---
+++ src/main/java/com/hendisantika/springbootexcelcsvpdf/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.hendisantika.springbootexcelcsvpdf.controller;
+
+import com.hendisantika.springbootexcelcsvpdf.model.User;
+import com.hendisantika.springbootexcelcsvpdf.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-excel-csv-pdf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/02/18
+ * Time: 06.31
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    /**
+     * Handle request to the default page
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<User> viewHome() {
+        return userService.findAllUsers();
+    }
+}
