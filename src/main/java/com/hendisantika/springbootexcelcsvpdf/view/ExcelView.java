ac0bc0d5f21f4220ef893010cbde8b993ec3860d
Hendi Santika <hendisantika@gmail.com>
2018-02-19 21:00:35 +0700

    #6 - Add Excel View

---
+++ src/main/java/com/hendisantika/springbootexcelcsvpdf/view/ExcelView.java
@@ -0,0 +1,93 @@
+package com.hendisantika.springbootexcelcsvpdf.view;
+
+import com.hendisantika.springbootexcelcsvpdf.model.User;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.*;
+import org.springframework.web.servlet.view.document.AbstractXlsView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-excel-csv-pdf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/02/18
+ * Time: 20.59
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class ExcelView extends AbstractXlsView {
+
+    @Override
+    protected void buildExcelDocument(Map<String, Object> model,
+                                      Workbook workbook,
+                                      HttpServletRequest request,
+                                      HttpServletResponse response) {
+
+        // change the file name
+        response.setHeader("Content-Disposition", "attachment; filename=\"my-xls-file.xls\"");
+
+        @SuppressWarnings("unchecked")
+        List<User> users = (List<User>) model.get("users");
+
+        // create excel xls sheet
+        Sheet sheet = workbook.createSheet("User Detail");
+        sheet.setDefaultColumnWidth(30);
+
+        // create style for header cells
+        CellStyle style = workbook.createCellStyle();
+        Font font = workbook.createFont();
+        font.setFontName("Arial");
+        style.setFillForegroundColor(HSSFColor.BLUE.index);
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        font.setBold(true);
+        font.setColor(HSSFColor.WHITE.index);
+        style.setFont(font);
+
+
+        // create header row
+        Row header = sheet.createRow(0);
+        header.createCell(0).setCellValue("Firstname");
+        header.getCell(0).setCellStyle(style);
+        header.createCell(1).setCellValue("LastName");
+        header.getCell(1).setCellStyle(style);
+        header.createCell(2).setCellValue("Age");
+        header.getCell(2).setCellStyle(style);
+        header.createCell(3).setCellValue("Job Title");
+        header.getCell(3).setCellStyle(style);
+        header.createCell(4).setCellValue("Company");
+        header.getCell(4).setCellStyle(style);
+        header.createCell(5).setCellValue("Address");
+        header.getCell(5).setCellStyle(style);
+        header.createCell(6).setCellValue("City");
+        header.getCell(6).setCellStyle(style);
+        header.createCell(7).setCellValue("Country");
+        header.getCell(7).setCellStyle(style);
+        header.createCell(8).setCellValue("Phone Number");
+        header.getCell(8).setCellStyle(style);
+
+
+        int rowCount = 1;
+
+        for (User user : users) {
+            Row userRow = sheet.createRow(rowCount++);
+            userRow.createCell(0).setCellValue(user.getFirstName());
+            userRow.createCell(1).setCellValue(user.getLastName());
+            userRow.createCell(2).setCellValue(user.getAge());
+            userRow.createCell(3).setCellValue(user.getJobTitle());
+            userRow.createCell(4).setCellValue(user.getCompany());
+            userRow.createCell(5).setCellValue(user.getAddress());
+            userRow.createCell(6).setCellValue(user.getCity());
+            userRow.createCell(7).setCellValue(user.getCountry());
+            userRow.createCell(8).setCellValue(user.getPhoneNumber());
+
+        }
+
+    }
+
+}
