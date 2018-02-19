b56d69bbddb1d503f5552d17663704c25ef9c6c2
Hendi Santika <hendisantika@gmail.com>
2018-02-19 20:30:39 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootexcelcsvpdf/SpringBootExcelCsvPdfApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootexcelcsvpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExcelCsvPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExcelCsvPdfApplication.class, args);
+	}
+}
