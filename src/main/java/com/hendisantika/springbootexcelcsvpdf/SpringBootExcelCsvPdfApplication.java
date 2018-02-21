5a2b37ce4a21ba1d601c8c542ca16328e4922d5a
Hendi Santika <hendisantika@gmail.com>
2018-02-21 08:29:55 +0700

    #19 - Add Download 4 & 5 API

--- src/main/java/com/hendisantika/springbootexcelcsvpdf/SpringBootExcelCsvPdfApplication.java
+++ src/main/java/com/hendisantika/springbootexcelcsvpdf/SpringBootExcelCsvPdfApplication.java
@@ -2,6 +2,9 @@ package com.hendisantika.springbootexcelcsvpdf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 
 @SpringBootApplication
 public class SpringBootExcelCsvPdfApplication {
@@ -9,4 +12,10 @@ public class SpringBootExcelCsvPdfApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExcelCsvPdfApplication.class, args);
 	}
+
+    @Bean
+    public HttpMessageConverters customConverters() {
+        ByteArrayHttpMessageConverter arrayHttpMessageConverter = new ByteArrayHttpMessageConverter();
+        return new HttpMessageConverters(arrayHttpMessageConverter);
+    }
 }
--- src/main/java/com/hendisantika/springbootexcelcsvpdf/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootexcelcsvpdf/controller/UserController.java
@@ -4,6 +4,7 @@ import com.hendisantika.springbootexcelcsvpdf.model.User;
 import com.hendisantika.springbootexcelcsvpdf.service.UserService;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -13,14 +14,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.List;
 
 /**
@@ -84,5 +83,39 @@ public class UserController {
                 .contentLength(file.length())
                 .contentType(MediaType.parseMediaType("application/octet-stream"))
                 .body(resource);
+    }
+
+    @GetMapping(value = "/download4", produces = "application/pdf")
+    public ResponseEntity<InputStreamResource> downloadPDFFile()
+            throws IOException {
+
+        ClassPathResource pdfFile = new ClassPathResource("Keyboard Shortcuts Mac.pdf");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        return ResponseEntity
+                .ok()
+                .contentLength(pdfFile.contentLength())
+                .contentType(
+                        MediaType.parseMediaType("application/octet-stream"))
+                .body(new InputStreamResource(pdfFile.getInputStream()));
+    }
+
+    @GetMapping("/download5")
+    public StreamingResponseBody getSteamingFile(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=\"Keyboard Shortcuts Mac.pdf\"");
+        InputStream inputStream = new FileInputStream(new File("/Users/hendisantika/Documents/IdeaProjects/spring-boot-excel-csv-pdf/src/main/resources/Keyboard Shortcuts Mac.pdf"));
+        return outputStream -> {
+            int nRead;
+            byte[] data = new byte[1024];
+            while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+                System.out.println("Writing some bytes..");
+                outputStream.write(data, 0, nRead);
+            }
+        };
     }
 }
