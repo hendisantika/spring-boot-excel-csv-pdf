5069decf063966f21b28f149630b30b61241179b
Hendi Santika <hendisantika@gmail.com>
2019-12-03 06:07:18 +0700

    #27 - Upgraded into Spring Boot 2.2.0.RELEASE version

--- src/main/java/com/hendisantika/springbootexcelcsvpdf/SpringBootExcelCsvPdfApplication.java
+++ src/main/java/com/hendisantika/springbootexcelcsvpdf/SpringBootExcelCsvPdfApplication.java
@@ -2,7 +2,7 @@ package com.hendisantika.springbootexcelcsvpdf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 
